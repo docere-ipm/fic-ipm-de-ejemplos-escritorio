694dca8783012870148d29c877c8ec1d20a3c765
David Cabrero Souto <david.cabrero@madsgroup.org>
2022-09-12 13:40:42 +0200

    Ventana y layout con más de un hijo

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -4,34 +4,70 @@ import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
+# En otros lenguajes, esto sería una constante.
+WINDOW_PADDING: int = 24
 
-# En GTK los manejadores de eventos, reciven un parámetro: el widget
-# en el que se produjo el evento.
-def on_button_clicked(w: Gtk.Widget) -> None:
+
+def on_say_hello_clicked(w: Gtk.Widget) -> None:
     print("Hello!")
-    
+
+
+# Si tenemos más de un widget dentro de la ventana, tenemos que
+# resolver el _layout_:
+#
+#   - ¿ Cómo indicamos la posición de cada uno ?
+#   - ¿ Cómo indicamos el tamaño de cada uno ?
+#
+# a. En entornos siempre iguales y conocidos (muy raro):
+#    coordenadas y tamaño en pixels
+#
+# b. _Containers_, _contenedores_: objetos que calculan el layout
+#    de sus hijos según un criterio.
+
+
+def counter() -> Gtk.Widget:
+    # `Box` es un contenedor básico, dispone sus hijos en línea,
+    # vertical u horizontal.
+    #
+    # En esta aplicación no se da el caso, pero un container puede ser
+    # hijo de otro container. Esto nos permite crear layouts
+    # complejos.
+    box = Gtk.Box(
+        orientation= Gtk.Orientation.VERTICAL,
+        homogeneous= False,
+        spacing= 12,
+        margin_top= WINDOW_PADDING,
+        margin_end= WINDOW_PADDING,
+        margin_bottom= WINDOW_PADDING,
+        margin_start= WINDOW_PADDING
+    )
+    label = Gtk.Label(
+        label= "Hello!",
+        halign= Gtk.Align.CENTER,
+        vexpand= True
+    )
+    button = Gtk.Button(
+        label= "Say Hello",
+        halign= Gtk.Align.CENTER
+    )
+    box.append(label)
+    box.append(button)
+    button.connect('clicked', on_say_hello_clicked)
+    return box
+
 
 def on_activate(app: Gtk.Application) -> None:
-    win = Gtk.ApplicationWindow(title= "¡Hola Mundo!",
-                                default_width= 600,
-                                default_height= 400)    
+    win = Gtk.ApplicationWindow(
+        title= "¡Hola Mundo!",
+    )    
     app.add_window(win)
     win.connect("destroy", lambda win: win.close())
-
-    # Añadimos un `Button`
-    # Los botones son widgets que al activarlos lanzan una acción.
+    # La parte que crea los widgets contenidos en la ventana la
+    # separamos a otra función para organizar el código.
     #
-    #   - activar = hacer click | pulsar espacio
-    #
-    #   - ¿ Cómo sabe la usuaria que es un botón ? ¿ y cómo sabe
-    #     que lanza una acción ?
-    #
-    #   - Si hay varios widgets, ¿ Cómo sabemos que pulsar espacio
-    #   - activa este botón ?
-    button = Gtk.Button(label= "Print hello")
-    button.connect('clicked', on_button_clicked)
-    win.set_child(button)
-    
+    # El código que crea la interface suele ser largo. La manera de
+    # organizarlo ya va en el estilo de programación.
+    win.set_child(counter())
     win.present()
 
 
