9840a4a48369c229adf7faa3919d8f0e9311bae0
David Cabrero Souto <david.cabrero@madsgroup.org>
2022-09-12 10:29:22 +0200

    Aplicación con una ventana

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 
-# En python usamos Gtk a traves de GObject Introspection.
-# https://gi.readthedocs.io/en/latest/ Es una idea muy interesante,
-# pero fuera del ámbito de las IGUs.
 import gi
-
-# Usaremos la versión 4 de Gtk. Se puede usar la versión 3 haciendo
-# los cambios pertienentes.
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 
+def on_activate(app: Gtk.Application) -> None:
+    # Es habitual que las librerías gráficas sigan un diseño OO, donde
+    # tenemos que crear una instancia por cada widget.
+    # Cada librería tiene su api y su galería de widgets.
+    
+    # Creamos una ventana, inicializando algunas de sus propiedades.
+    win = Gtk.ApplicationWindow(title= "¡Hola Mundo!",
+                                default_width= 600,
+                                default_height= 400)
+    
+    # Añadimos la ventana a la aplicación.
+    app.add_window(win)
+
+    # En GTK tenemos que programar explicitamente que un widget sea
+    # visible.
+    win.present()
+
+
 if __name__ == '__main__':
-    # Es habitual en muchas librerías gráficas tener un objeto que
-    # representa la aplicación.
     app = Gtk.Application(application_id= "es.udc.fic.ipm.HelloWorld")
 
-    # Una vez finalizada la inicialización de la aplicación, le
-    # pasamos el control a la librería.  Se podría decir que esta es
-    # la implementación del `main` en la librería.
+    # Antes de pasar el control a la librería, tenemos que establecer
+    # una función o método que se encarge de crear la interface.
+    app.connect('activate', on_activate)
     app.run(None)
