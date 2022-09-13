b1b8e47ca3e102eb5dcc116ff5887171cd7c952d
David Cabrero Souto <david.cabrero@madsgroup.org>
2022-09-13 21:17:41 +0200

    Internacionalizada la interface. Localizada al español. Localizada (mal) al árabe y chino.

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -4,14 +4,28 @@ from __future__ import annotations
 from typing import Optional
 
 
+# Vamos a internacionalizar la apliación añadiendo el soporte para
+# texto en distintos idiomas usando `gettext`.
+
+# Para el texto internacionalizado necesitamos la librería de soporte
+# de locales y gettext.
+import locale
+import gettext
+from pathlib import Path
+
+
 import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 
-# Vamos a aplicar el patrón MVP
+# Creamos un "alias" para las funciones de traducción con un nombre
+# más corto.
+# La convención es ' _' y 'N_'.
+_ = gettext.gettext
+N_ = gettext.ngettext
 
-# El componente estado/modelo se reduce a una clase.
+
 class State:
     count: int= 0
 
@@ -22,12 +36,13 @@ class State:
         return self.count
 
 
-# El componente vista se reduce a una clase y una función.
 def get_count_text(count: int) -> str:
-    return (
-        "I've said hello 1 time" if count == 1 else
-        f"I've said hello {count} times"
-    )
+    # Las f-strings no son internacionalizables
+    return N_(
+        "I've said hello {0} time",
+        "I've said hello {0} times",
+        count
+    ).format(count)
 
 
 class View:
@@ -35,12 +50,10 @@ class View:
 
     label: Gtk.Label = None
 
-    # Esta no es la única forma de implementar esta parte. Por
-    # ejemplo, en vez de pasar el Presenter como parámetro, podíamos
-    # tener un método `View.connect_to(:Presenter)`
     def build(self, app: Gtk.Application, presenter: Presenter) -> None:
         win = Gtk.ApplicationWindow(
-            title= "¡Hola Mundo!",
+            # Por defecto todos los textos están en ingles
+            title= _("Hello World!"),
         )    
         app.add_window(win)
         win.connect("destroy", lambda win: win.close())
@@ -63,7 +76,7 @@ class View:
             vexpand= True
         )
         button = Gtk.Button(
-            label= "Say Hello",
+            label= _("Say Hello"),
             halign= Gtk.Align.CENTER
         )
         box.append(label)
@@ -76,7 +89,6 @@ class View:
         self.label.set_label(get_count_text(count))
 
 
-# Finalmente el Presenter
 class Presenter:
     def __init__(self, state: Optional[State]= None):
         state = state or State()
@@ -90,14 +102,10 @@ class Presenter:
 
     def on_activate(self, app: Gtk.Application) -> None:
         self.view.build(app, self)
-        # La vista es totalmente pasiva. El presenter se encarga de
-        # actualizarla en todo momento.
         self._update_count()
         
     def on_say_hello_clicked(self, _w: Gtk.Widget) -> None:
-        # "Llamamos" al modelo
         self.state.incr_count()
-        # Actualizamos la vista
         self._update_count()
 
     def _update_count(self) -> None:
@@ -105,5 +113,52 @@ class Presenter:
 
         
 if __name__ == '__main__':
+    # Primero vamos a inicalizar el soporte de traducciones.
+
+    # El primer paso es configurar el módulo `locale` con el locale de
+    # la usuaria.
+    # En windows esta configuración no se hace con las variables de entorno.
+    locale.setlocale(locale.LC_ALL, '')
+
+    # Establecemos las BBDD de traducciones. En gettext se llaman _dominios_.
+    # Primero calculamos el directorio donde se encuentran.
+    # En un FHS sería `/usr/share/locale`, ...
+    LOCALE_DIR = Path(__file__).parent / "locale"
+    # Relacionamos el nombre de las BBDD y el directorio donde buscar.
+    # Las BBDD se llaman igual para todos los idiomas, sólo cambia el
+    # directorio donde están.
+    locale.bindtextdomain('HelloWorld', LOCALE_DIR)
+    gettext.bindtextdomain('HelloWorld', LOCALE_DIR)
+    # Configuramos el nombre de la BD de traducciones a usar
+    gettext.textdomain('HelloWorld')
+    
     presenter = Presenter()
     presenter.run()
+
+
+    # Una vez internacionalizado el código, tenemos que hacer las
+    # localizaciones.
+    #
+    # `xgettext -kN_:1,2 helloworld.py` extrae todas las cadenas de
+    # texto del código que necesitan una traducción, incluyendo las
+    # que tienen forma en plural.
+    # Posiblemente tengamos que editar el fichero messages.po para
+    # establecer el CHARSET.
+    #
+    # A continuación la persona encargada realiza las traducciones.
+    # El fichero de traducciones para cada idioma se inicializa:
+    # `$ msginit -i messages.po -l es`
+    # o se actualiza con `msmerge`
+    #
+    # Una vez traducido, hay que crear la BD y copiarla al directorio
+    # correspondiente. P.e.: {LOCALE_DIR}/es/LC_MESSAGES
+    # El fichero con la BD tiene que tener el mismo nombre que usamos
+    # en la configuración de gettext (nombre de dominio)
+    # `msgfmt es.po -o locale/es/LC_MESSAGES/HelloWorld.mo`
+
+    # En Unix la configuración del locale se establece en las
+    # variables de entorno. Ver `locale -a`, y `locale`
+    #
+    # Para probar una localización es suficiente con cambiar esas variables.
+    # P.e.: (según la shell y locale)
+    # `LC_ALL=en_GB.utf8 ./helloworld.py`
